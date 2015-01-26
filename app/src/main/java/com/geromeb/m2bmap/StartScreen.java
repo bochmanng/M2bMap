32ba946e258916ddbf84f00df637af9787c11a97
Gerome Bochmann <gerome.bochmann@gmail.com>
2015-01-26 23:36:34 +0100

    Added comments and added another removecallback() before invoking mHandler.post(mUpdateTimeTask). Also, made cdText invisible on startup.

--- app/src/main/java/com/geromeb/m2bmap/StartScreen.java
+++ app/src/main/java/com/geromeb/m2bmap/StartScreen.java
@@ -64,11 +64,9 @@ public class StartScreen extends FragmentActivity {
     }
 
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
+     * This method creates two markers on startup and realizes the camera animation to the Berlin marker.
+     * Finally, it instantiates a listener for marker clicks.
+     * */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(berlin));
         mMap.addMarker(new MarkerOptions().position(munich));
@@ -98,21 +96,33 @@ public class StartScreen extends FragmentActivity {
 
     }
 
+    /**
+     * This method mediates the camera movement. It's just a shortcut.
+     * */
+
+
     private void moveToNewLocation(LatLng newLocation) {
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(newLocation, 10), 5000, null);
 
     }
 
+    /**
+     * This method implements the counter and the subsequent camera animation.
+     * */
+
     private void countToLocation(final LatLng newLocation) {
 
+        // Access the TextView we will use to represent the counter. Set the counter to 6, and make the TextView visible.
         final TextView cdText = (TextView) findViewById(R.id.cdText);
-
         final int[] t = {6};
-        final Handler mHandler = new Handler(Looper.getMainLooper());
-
         cdText.setVisibility(View.VISIBLE);
 
+
+        // This handler makes sure the countdown is displayed smoothly.
+        final Handler mHandler = new Handler(Looper.getMainLooper());
+        // This runnable is instantiated as an anonymous class. It calls its run method every 1000ms.
+        // The run method counts backwards from 6 to 0 and sends the current number to cdText.
         Runnable mUpdateTimeTask = new Runnable() {
             @Override
             public void run() {
@@ -123,6 +133,7 @@ public class StartScreen extends FragmentActivity {
                 if (t[0] == 0) {
                     cdText.setVisibility(View.INVISIBLE);
                     moveToNewLocation(newLocation);
+                    // Make sure there are no callbacks to mUpdateTimeTask left.
                     mHandler.removeCallbacks(this);
                 } else {
                     mHandler.postDelayed(this, 1000);
@@ -130,9 +141,12 @@ public class StartScreen extends FragmentActivity {
             }
         };
 
+        // Make sure there are no callbacks to mUpdateTimeTask left and then start the countdown.
+        mHandler.removeCallbacks(mUpdateTimeTask);
         mHandler.post(mUpdateTimeTask);
 
 
+        // This variant of the countdown works but sometimes numbers are skipped.
 /*        new CountDownTimer(5000, 1000) {
 
             @Override
